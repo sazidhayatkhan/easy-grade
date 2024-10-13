d646931c946da36e10e0b1c70550be6ef1e7ea91
sazidhayatkhan <sazidhayatkhan@gmail.com>
2024-10-14 01:11:26 +0600

    fi

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -13,11 +13,11 @@ const Modal: React.FC<Props> = (props) => {
   return (
     <div>
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button className="btn" onClick={openModal}>
+      <button className="bg-transparent" onClick={openModal}>
         How to use
       </button>
       <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
+        <div className="modal-box bg-white">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
