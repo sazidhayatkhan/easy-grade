500c00cf74a7367b2cf504117ad7d169dfa441e0
sazidhayatkhan <sazidhayatkhan@gmail.com>
2024-10-16 16:43:01 +0600

    fixed homepage

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -36,7 +36,7 @@ const DrawerUI = ({ buttonContent }: Props) => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <ul className="menu bg-white text-black min-h-full w-80 p-4">
           {/* Sidebar content here */}
           {menuData?.map((item: any, i: any) => (
             <div key={i}>
