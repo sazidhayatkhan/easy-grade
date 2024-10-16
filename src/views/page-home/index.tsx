3bebc6b604ee0c4ce312dbe955c785d4eedd1552
sazidhayatkhan <sazidhayatkhan@gmail.com>
2024-10-16 13:53:39 +0600

    fixed homepage

--- src/views/page-home/index.tsx
+++ src/views/page-home/index.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 const HomeUI = (props: Props) => {
   return (
-    <div className='bg-primary min-h-screen'>
-      <div className='w-[90%] md:w-[70%] mx-auto flex justify-center items-center pt-[150px]'>
+    <div className='bg-primary h-[calc(100vh-100px)] flex justify-center items-center'>
+      <div className='w-[90%] md:w-[70%]'>
         <p className='text-center text-white text-6xl md:text-8xl font-bold italic anton uppercase'>Let's make our life a bit easy</p>
       </div>
     </div>
