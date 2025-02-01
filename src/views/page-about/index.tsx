4e52bccc9504368e97d5806c4c431d1f24a2918f
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-02-01 23:19:18 +0600

    testing

--- src/views/page-about/index.tsx
+++ src/views/page-about/index.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const AboutUI = (props: Props) => {
   return (
-    <div className="h-[calc(100vh-190px)] md:h-[calc(100vh-100px)] w-[90%] md:w-[60%] mx-auto flex justify-center items-center">
+    <div className="h-[calc(100vh-120px)] md:h-[calc(100vh-100px)] w-[90%] md:w-[60%] mx-auto flex justify-center items-center">
       <div>
         <h1 className="text-2xl font-bold text-center mb-4 text-gray-800">
           About the App
